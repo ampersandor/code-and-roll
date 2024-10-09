360c45fee99b00c8801622e0fd356988ae580dc8
DongHunKim <57800485+ampersandor@users.noreply.github.com>
2024-10-10 08:49:58 +0900

    [Gold II] Title: 우수 마을, Time: 76 ms, Memory: 37248 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -0,0 +1,28 @@
+from sys import stdin, setrecursionlimit
+from collections import defaultdict
+input = stdin.readline
+setrecursionlimit(10**7)
+n = int(input())
+graph = defaultdict(list)
+weights = list(map(int, input().split()))
+dp = [[0, weights[i]] for i in range(n)]
+
+for _ in range(n-1):
+    a, b = map(lambda x: int(x) - 1, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+
+def dfs(node, parent):
+    for child in graph[node]:
+        if parent != child:
+            dfs(child, node)
+            dp[node][0] += max(dp[child])
+            dp[node][1] += dp[child][0]
+
+dfs(0, 0)
+
+
+print(max(dp[0]))
+
