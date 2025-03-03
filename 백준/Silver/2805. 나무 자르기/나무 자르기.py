0b6bf87bec1044e034714cdf50c6fddcfaeacb6f
DongHunKim <ddong3525@naver.com>
2025-03-03 11:50:45 +0900

    [Silver II] Title: 나무 자르기, Time: 376 ms, Memory: 227788 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,15 +1,17 @@
 from bisect import bisect_left
+from collections import Counter
 
 n, m = map(int, input().split())
 
-trees = sorted(map(int, input().split()))
+trees = Counter(map(int, input().split()))
 
+keys = sorted(trees.keys())
 
 def calc(k):
     res = 0
-    idx = bisect_left(trees, k)
-    for i in range(idx, len(trees)):
-        res += trees[i] - k
+    idx = bisect_left(keys, k)
+    for i in range(idx, len(keys)):
+        res += (keys[i] - k) * trees[keys[i]]
     return res
 
 def solve():
