181ebef3b71d988faa37416a4facbcc6acfd6687
DongHunKim <ddong3525@naver.com>
2025-03-06 08:49:48 +0900

    [Silver II] Title: 좌표 압축, Time: 944 ms, Memory: 301268 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,8 @@
-from collections import Counter
-
 n = int(input())
-nums = list(map(int, input().split()))
-counter = Counter(nums)
+arr = list(map(int, input().split()))
+
 hashed = dict()
-for i, k in enumerate(sorted(counter.keys())):
+for i, k in enumerate(sorted(set(arr))):
     hashed[k] = str(i)
 
-print(" ".join(map(lambda x: hashed[x], nums)))
+print(" ".join(map(lambda x: hashed[x], arr)))
