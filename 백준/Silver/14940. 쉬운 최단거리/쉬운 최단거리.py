5a2f3c36b25ee06d297146a6c6194e223803b50c
DongHunKim <ddong3525@naver.com>
2025-03-14 09:22:09 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 216 ms, Memory: 122528 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,25 +1,14 @@
 from collections import deque
+from sys import stdin, stdout
 
+read = stdin.readline
+write = stdout.write
 
-n, m = map(int, input().split())
 DELTA = (0, 1, 0, -1, 0)
 
-grid = []
-res = [[-1] * m for _ in range(n)]
-start: tuple = ()
-visited = [[False] * m for _ in range(n)]
-
-for i in range(n):
-    row = list(map(int, input().split()))
-    grid.append(row)
-    for j in range(m):
-        if row[j] == 2:
-            start = i, j
-        elif row[j] == 0:
-            res[i][j] = 0
-
-
-def bfs(x, y):
+def bfs(x, y, n, m, res, grid):
+    visited = [[False] * m for _ in range(n)]
+    visited[x][y] = True
     que = deque([(x, y, 0)])
     while que:
         i, j, c = que.popleft()
@@ -30,7 +19,23 @@ def bfs(x, y):
                 visited[ni][nj] = True
                 que.append((ni, nj, c + 1))
 
-visited[start[0]][start[1]] = True
-bfs(*start)
-for row in res:
-    print(*row)
+def solve():
+    n, m = map(int, read().split())
+    grid = []
+    res = [[-1] * m for _ in range(n)]
+    sx, sy = 0, 0
+
+    for i in range(n):
+        row = list(map(int, read().split()))
+        grid.append(row)
+        for j in range(m):
+            if row[j] == 2:
+                sx, sy = i, j
+            elif row[j] == 0:
+                res[i][j] = 0
+    bfs(sx, sy, n, m, res, grid)
+    for row in res:
+        write(" ".join(map(str, row)) + "\n")
+
+
+solve()
