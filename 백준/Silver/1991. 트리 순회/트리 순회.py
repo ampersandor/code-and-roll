2131e1cb3bbabf381b129560fe1c60e2c3191aeb
DongHunKim <ddong3525@naver.com>
2025-03-19 08:58:26 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 112308 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,42 @@
+from collections import namedtuple
+
+Node = namedtuple("Node", ["name", "left", "right"])
+
+n = int(input())
+tree = dict()
+for _ in range(n):
+    node, left, right = input().strip().split()
+    tree[node] = Node(node, left, right)
+
+in_ = []
+pre = []
+post = []
+
+def inorder(v: str):
+    if v == ".":
+        return
+    in_.append(v)
+    inorder(tree[v].left)
+    inorder(tree[v].right)
+
+def preorder(v: str):
+    if v == ".":
+        return
+    preorder(tree[v].left)
+    pre.append(v)
+    preorder(tree[v].right)
+
+def postorder(v: str):
+    if v == ".":
+        return
+    postorder(tree[v].left)
+    postorder(tree[v].right)
+    post.append(v)
+
+inorder("A")
+preorder("A")
+postorder("A")
+
+print("".join(in_))
+print("".join(pre))
+print("".join(post))
