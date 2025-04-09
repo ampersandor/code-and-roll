ce216fe9c605c2509f1ffc9fc188b661271f53e6
DongHunKim <ddong3525@naver.com>
2025-04-09 16:11:39 +0900

    [Gold IV] Title: 알파벳, Time: 4124 ms, Memory: 147400 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,20 +1,33 @@
-from collections import deque
+import sys
 
 n, m = map(int, input().split())
 
 grid = [list(input().strip()) for _ in range(n)]
-visited = set(grid[0][0])
+visited = [0] * 26
+visited[ord(grid[0][0]) - ord('A')] = 1
 DELTA = (0, 1, 0, -1, 0)
 res = 0
 
-def find(i, j):
+
+def find(i, j, count):
     global res
-    res = max(res, len(visited))
+    res = max(res, count)
+    if res == 26:
+        print(res)
+        sys.exit(0)
     for d in range(4):
         ni, nj = i + DELTA[d], j + DELTA[d+1]
-        if 0 <= ni < n and 0 <= nj < m and grid[ni][nj] not in visited:
-            visited.add(grid[ni][nj])
-            find(ni, nj)
-            visited.remove(grid[ni][nj])
-find(0, 0)
+        if 0 <= ni < n and 0 <= nj < m and not visited[ord(grid[ni][nj]) - ord('A')]:
+            visited[ord(grid[ni][nj]) - ord('A')] = 1
+            find(ni, nj, count + 1)
+            visited[(ord(grid[ni][nj]) - ord('A'))] = 0
+find(0, 0, 1)
 print(res)
+
+# {'H', 'G', 'D', 'A', 'F', 'B', 'J'}
+# 3 6
+# HFDFFB
+# AJHGDH
+# DGAGEH
+#
+# HFJADG
