31acb15251d61777244dea56fb36fd9f70ddba00
DongHunKim <ddong3525@naver.com>
2025-04-18 13:23:09 +0900

    [Gold III] Title: 파티, Time: 192 ms, Memory: 116188 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -4,19 +4,18 @@ from heapq import heappush, heappop
 n, m, x = map(int, input().split())
 
 edges = defaultdict(dict)
-
+rev_edges = defaultdict(dict)
 INF = float('inf')
 
 for _ in range(m):
     a, b, c = map(int, input().split())
     edges[a][b] = min(edges[a].get(b, 100), c)
+    rev_edges[b][a] = min(rev_edges[b].get(a, 100), c)
 
-
-def dijkstra(start):
+def dijkstra(start, edges):
     heap = [(0, start)]
     dist = [INF] * (n + 1)
     dist[start] = 0
-
     while heap:
         cost, node = heappop(heap)
         for child, nc in edges[node].items():
@@ -27,9 +26,12 @@ def dijkstra(start):
 
     return dist
 
-dist = dijkstra(x)
+dist1 = dijkstra(x, edges)
+dist2 = dijkstra(x, rev_edges)
+
+res = 0
 for i in range(1, n+1):
-    dist[i] += dijkstra(i)[x]
+    if i != x:
+        res = max(res, dist1[i] + dist2[i])
 
-print(max(dist[1:]))
-
+print(res)
