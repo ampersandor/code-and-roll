0e67db3174aa6cde53a08e16129c2df320fb7ab7
DongHunKim <ddong3525@naver.com>
2025-07-29 22:25:24 +0900

    [Silver II] Title: 스택 수열, Time: 236 ms, Memory: 21776 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,27 +5,34 @@ import java.util.*;
 
 public class Main{
 
+    public static int read() throws IOException {
+        int num = System.in.read() & 15;
+        int input;
+        while ((input = System.in.read()) > 32) {
+            num = ((num << 3) + (num << 1)) + (input & 15);
+        }
+        return num;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = read();
         Deque<Integer> stack = new ArrayDeque<>();
         int[] target = new int[n];
         int ptr = 0;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++){
-            target[i] = Integer.parseInt(br.readLine());
+            target[i] = read();
         }
-
         for (int i = 1; i <= n; i++) {
             if (i <= target[ptr]) {
                 stack.push(i);
-                sb.append("+\n");
+                sb.append("+").append("\n");
             } else {
                 break;
             }
             while (!stack.isEmpty() && stack.peek() == target[ptr]) {
-                int a = stack.pop();
-                sb.append("-\n");
+                stack.pop();
+                sb.append("-").append("\n");
                 ptr++;
             }
         }
