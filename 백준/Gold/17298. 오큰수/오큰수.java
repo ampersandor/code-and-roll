712afa5c765010c57c4cc9ee2a319939568112f5
DongHunKim <ddong3525@naver.com>
2025-08-01 14:48:03 +0900

    [Gold IV] Title: 오큰수, Time: 584 ms, Memory: 71408 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,26 +1,27 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int c, n;
+    static int read() throws Exception {
+        n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    public static void main(String[] args) throws Exception {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        Deque<Integer> stack = new ArrayDeque<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = read();
+        int[] stack = new int[n];
+        int ptr = 0;
         int[] nums = new int[n];
         int[] res = new int[n];
 
         for (int i = 0; i < n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]){
-                int idx = stack.pop();
-                res[idx] = nums[i];
+            nums[i] = read();
+            while (ptr > 0 && nums[stack[ptr-1]] < nums[i]){
+                res[stack[--ptr]] = nums[i];
             }
-            stack.push(i);
+            stack[ptr++] = i;
         }
 
         for (int i = 0; i < n; i++){
