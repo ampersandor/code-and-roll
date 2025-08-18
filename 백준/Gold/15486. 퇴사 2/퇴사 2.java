7017d1dd54b9ab2d5a818a1fdc4b6f14937c9144
DongHunKim <ddong3525@naver.com>
2025-08-18 19:52:38 +0900

    [Gold V] Title: 퇴사 2, Time: 668 ms, Memory: 316004 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,19 +10,22 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] times = new int[n];
         int[] costs = new int[n];
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n+1];
         for (int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             times[i] = Integer.parseInt(st.nextToken());
             costs[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i <= n; i++){
-            dp[i] = Math.max(dp[i], dp[i-1]);
-            int next = i + times[i-1] - 1;
+        for (int i = 0; i < n; i++){
+            if (i > 0) {
+                dp[i] = Math.max(dp[i], dp[i-1]);
+            }
+            int next = i + times[i];
             if (next <= n) {
-                dp[next] = Math.max(dp[next], dp[i-1] + costs[i-1]);
+                dp[next] = Math.max(dp[next], dp[i] + costs[i]);
             }
         }
+        dp[n] = Math.max(dp[n-1], dp[n]);
 
         System.out.println(dp[n]);
 
