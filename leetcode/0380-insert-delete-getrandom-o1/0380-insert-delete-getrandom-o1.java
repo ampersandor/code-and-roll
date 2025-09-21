97fb834568177e027aa0c0fd3c212ea97ae60e67
DongHunKim <ddong3525@naver.com>
2025-09-21 18:01:46 +0900

    Time: 27 ms (71.77%), Space: 92.6 MB (19.77%) - LeetHub

--- leetcode/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ leetcode/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,33 +1,40 @@
 class RandomizedSet {
-    private Set<Integer> set;
-    private Random random;
-    private int n;
-    public RandomizedSet() {
-        this.set = new HashSet<>();
-        this.random = new Random();
-        
-    }
-    
-    public boolean insert(int val) {
-        return this.set.add(val);
-        
-    }
-    
-    public boolean remove(int val) {
-        return this.set.remove(val);
-        
-    }
-    
-    public int getRandom() {
 
-        return (Integer) this.set.toArray()[this.random.nextInt(this.set.size())];
+    private List<Integer> values;
+    private Map<Integer, Integer> valuesIdx;
+
+    public RandomizedSet() {
+        this.values = new ArrayList<>();
+        this.valuesIdx = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (valuesIdx.containsKey(val)) {
+            return false;
+        }
+
+        valuesIdx.put(val, values.size());
+        values.add(val);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!valuesIdx.containsKey(val)) {
+            return false;
+        }
+
+        int index = valuesIdx.get(val);
+        valuesIdx.put(values.get(values.size() - 1), index);
+        valuesIdx.remove(val);
+        values.set(index, values.get(values.size() - 1));
+        values.remove(values.size() - 1);
+
+        return true;
+    }
+
+    public int getRandom() {
+        int index = (int) (Math.random() * values.size());
+        return values.get(index);
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
